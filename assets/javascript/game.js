7004d8dee132914a6845f7918c2bf25450a1fc59
David Koeller <david.koeller@gmail.com>
2022-01-16 22:33:20 -0600

    adding javascript functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var factions = ["The Arborec", "The Barony of Letnev", "The Clan of Saar", "The Embers of Muaat", "The Emirates of Hacan", "The Federation of Sol", "The Ghosts of Creuss", "The Mentak Coalition", "The Naalu Collective", "The Nekro Virus", "Sadakk N'orr", "The Universities of Jol-Nar", "The Winnu", "The Xxcha Kingdom", "The Yin Brotherhood", "The Yssaril Tribes"];
+var colors =["Green", "Blue", "Yellow", "Red", "Purple", "Black"];
+
+
+$("#start-button").on("click", function(){
+
+    $("#welcome").hide();
+
+    
+});
